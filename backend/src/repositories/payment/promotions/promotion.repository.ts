33a4f4e2e8251d5a654df533426f71886ebb733d
Rodrigo Moura <rrm2@cin.ufpe.br>
2023-08-04 20:30:27 -0300

    chore(Promotions): Correct promotion function names of repository file

--- backend/src/repositories/payment/promotions/promotion.repository.ts
+++ backend/src/repositories/payment/promotions/promotion.repository.ts
@@ -6,26 +6,26 @@ class PromotionRepository extends BaseRepository<PromotionEntity> {
     super('promotion');
   }
 
-  public async getTests(): Promise<PromotionEntity[]> {
+  public async getPromotions(): Promise<PromotionEntity[]> {
     return await this.findAll();
   }
 
-  public async getTest(id: string): Promise<PromotionEntity | null> {
+  public async getPromotion(id: string): Promise<PromotionEntity | null> {
     return await this.findOne((item) => item.id === id);
   }
 
-  public async createTest(data: PromotionEntity): Promise<PromotionEntity> {
+  public async createPromotion(data: PromotionEntity): Promise<PromotionEntity> {
     return await this.add(data);
   }
 
-  public async updateTest(
+  public async updatePromotion(
     id: string,
     data: PromotionEntity
   ): Promise<PromotionEntity | null> {
     return await this.update((item) => item.id === id, data);
   }
 
-  public async deleteTest(id: string): Promise<void> {
+  public async deletePromotion(id: string): Promise<void> {
     await this.delete((item) => item.id !== id);
   }
 }
