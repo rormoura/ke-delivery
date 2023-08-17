ea861462b4b92c2cc62bdd1d2f9d12e4e193769c
marcusv-fs <marcusv_fs@hotmail.com>
2023-08-17 13:10:24 -0300

    test(Pedidos): add pedidos to di

--- backend/src/di/index.ts
+++ backend/src/di/index.ts
@@ -1,5 +1,7 @@
 import OtherRepository from '../repositories/other.repository';
 import TestRepository from '../repositories/test.repository';
+import PedidosRepository from '../repositories/Pedidos/pedido.repository';
+import PedidosService from '../services/Pedidos/pedido.service';
 import TestService from '../services/test.service';
 import Injector from './injector';
 
@@ -13,5 +15,14 @@ di.registerService(
   new TestService(
     di.getRepository(TestRepository),
     di.getRepository(OtherRepository)
+  )
+);
+
+//Pedidos
+di.registerRepository(PedidosRepository, new PedidosRepository());
+di.registerService(
+  PedidosService,
+  new PedidosService(
+    di.getRepository(PedidosRepository),
   )
 );
