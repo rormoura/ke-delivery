1629500326103561f844441e79144917b1928ddf
marcusv-fs <marcusv_fs@hotmail.com>
2023-08-17 13:12:07 -0300

    test(Pedidos): add pedido's routes to routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,6 +2,8 @@ import { Express, Router } from 'express';
 import { di } from '../di';
 import TestController from '../controllers/test.controller';
 import TestService from '../services/test.service';
+import PedidoController from '../controllers/Pedidos/pedido.controller';
+import PedidoService from '../services/Pedidos/pedido.service';
 
 const router = Router();
 const prefix = '/api';
@@ -10,5 +12,9 @@ export default (app: Express) => {
   app.use(
     prefix,
     new TestController(router, di.getService(TestService)).router
+  );
+  app.use(
+    prefix,
+    new PedidoController(router, di.getService(PedidoService)).router
   );
 };
