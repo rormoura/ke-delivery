103834500dab9093a5f9fff67e2dfe6dd39f33e8
marinavelosom <engmarinamelo@gmail.com>
2023-09-11 20:29:17 -0300

    feature/restaurant: fix details in front

--- frontend/src/app/home/pages/Navbar/index.tsx
+++ frontend/src/app/home/pages/Navbar/index.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return(
     <section className={styles.container}>
       <div className={styles.logo}>
-        <img src={LogoTemporaria} alt="Logo"/>
+        <img src={LogoTemporaria} alt="Logo" className={styles.icon}/>
         <h1 className={styles.title}>Ké Delivery</h1>
       </div>
       <a href="/login">
