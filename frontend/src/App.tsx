5394f763ef0698c90f76732601f06f587d161135
marinavelosom <engmarinamelo@gmail.com>
2023-09-11 10:55:57 -0300

    feature/restaurant: start front de login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,25 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import CreateTest from "./app/home/pages/CreateTest";
+// import CreateTest from "./app/home/pages/CreateTest";
 import ListTests from "./app/home/pages/ListTests";
+import Login from "./app/home/pages/Login";
 
 const router = createBrowserRouter([
-  {
-    path: "*",
-    Component: CreateTest,
-  },
-  {
-    path: "/create-test",
-    Component: CreateTest,
-  },
+  // {
+  //   path: "*",
+  //   Component: CreateTest,
+  // },
+  // {
+  //   path: "/create-test",
+  //   Component: CreateTest,
+  // },
   {
     path: "/tests",
     Component: ListTests,
   },
+  {
+    path: "login",
+    Component: Login,
+  }
 ]);
 
 export default function App() {
