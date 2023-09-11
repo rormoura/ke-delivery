3bbcb0a5adfe7a4d96fa7d3691fa6c9298a5eab0
mario8978 <mmotal8978@gmail.com>
2023-09-11 14:41:36 -0300

    feat: front of the customer login screen made

--- frontend/src/app/home/pages/LoginCustomer/index.tsx
+++ frontend/src/app/home/pages/LoginCustomer/index.tsx
@@ -7,7 +7,12 @@ const LoginCustomer = () => {
     <section className={styles.container}>
       <img className={styles.imagem1} src={Food1} alt="Comida" />
       <div className={styles.content}> 
-        <h1 className={styles.title}>Ke delivery</h1>
+        <h1 className={styles.title}>O que você quer, onde você quer, quando você quer!</h1>
+        <input className={styles.input} type="text" placeholder="Digite seu e-mail" />
+        <input className={styles.input} type="password" placeholder="Digite sua senha" />
+        <button className={styles.button}>
+          <p className={styles.textButton}>Entrar</p>
+        </button>
       </div>
     </section>
   );
