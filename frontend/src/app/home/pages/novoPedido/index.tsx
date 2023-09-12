c0d724020c42bfccac2b727bf848daf18d0f132c
marcusv-fs <marcusv_fs@hotmail.com>
2023-09-12 11:11:35 -0300

    feat(NovoPediddo): Create NovoPedido page and css

--- frontend/src/app/home/pages/novoPedido/index.tsx
+++ frontend/src/app/home/pages/novoPedido/index.tsx
@@ -1,68 +1,18 @@
-import { SubmitHandler, useForm } from "react-hook-form";
-import styles from "./index.module.css";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useContext, useEffect } from "react";
-import { HomeContext } from "../../context/HomeContext";
-import { TestFormSchema, TestFormType } from "../../forms/TestForm";
-import Button from "../../../../shared/components/Pedidos/Button";
-import { Link } from "react-router-dom";
+import styles from "./NovoPedido.module.css";
+import Header from "../../../../shared/components/Pedidos/Header";
+import Footer from "../../../../shared/components/Pedidos/Footer";
+import Produtos from "../../../../shared/components/Pedidos/Produtos";
+
 
 
 const NovoPedido = () => {
-    const { state, prevState, service,  } = useContext(HomeContext);
-
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm<TestFormType>({
-    resolver: zodResolver(TestFormSchema),
-  });
-
-  const onSubmit: SubmitHandler<TestFormType> = async (body) => {
-    service.createTest(body);
-    reset();
-  };
-
-  useEffect(() => {
-    if (
-      state.createTestRequestStatus !== prevState?.createTestRequestStatus &&
-      state.createTestRequestStatus.isSuccess()
-    ) {
-      alert("Teste criado com sucesso!");
-    }
-  }, [state, prevState]);
-
-
     return (
-
-        <section className={styles.container}>
-            <h1>Meu Carrinho</h1>
-            <form className={styles.formContainer} onSubmit={handleSubmit(onSubmit)}>
-                <input
-                    data-cy="input-name"
-                    {...register("name")}
-                    placeholder="Observações"
-                    className={styles.formInputContainer}
-                />
-                {errors.name && (
-                    <span data-cy="input-name-error" className={styles.formError}>
-                        {errors.name.message}
-                    </span>
-                )}
-            </form>
-            
-            <Button type="submit" data-cy="create">
-                Prosseguir para pagamentoão
-            </Button>
-            <Button data-cy="create" type="reset">
-                Limpar carrinho
-            </Button>
-
-    </section>
-
-  );
+        <div className={styles.body}>
+            <Header></Header>
+            <Produtos/>
+            <Footer></Footer>
+        </div>
+    );
 };
 
 export default NovoPedido;
