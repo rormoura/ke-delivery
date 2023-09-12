d017379ead1eec7559fc5c8340dd24d9c2410645
Lucasnb2001 <lnb@cin.ufpe.br>
2023-09-12 16:52:31 -0300

    feat: implementa página principal de cardápio.

--- frontend/src/app/home/pages/Menu/index.tsx
+++ frontend/src/app/home/pages/Menu/index.tsx
@@ -1,11 +1,35 @@
 import styles from "./index.module.css";
 import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// register Swiper custom elements
+register();
 
 
 const Menu: React.FC = () => {
   const [data, setData] = useState<any>(null);
   const [loading, setLoading] = useState<boolean>(true);
+  const [restaurantData, setRestaurantData] = useState<any>(null); // Estado para os dados do restaurante
+
+  useEffect(() => {
+    const restaurantApiUrl = 'http://localhost:5001/api/restaurants'; 
+
+    fetch(restaurantApiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Erro na requisição');
+        }
+        return response.json();
+      })
+      .then((responseData) => {
+        setRestaurantData(responseData);
+      })
+      .catch((error) => {
+        console.error('Erro:', error);
+      });
+  }, []);
 
   useEffect(() => {
     // Substitua a URL abaixo pela URL da sua API
@@ -38,34 +62,37 @@ const Menu: React.FC = () => {
 
   // Renderize os dados recebidos na sua interface
   return (
-    <div >
-      <div className="flex items-center justify-center mt-10">
-        <img src="src\shared\assets\images\Subtract.png" alt="Imagem" className="mr-4" />
-        <h1 className="text-xl font-bold">Cardápio do restaurante X</h1>
-      </div>
-      <Link className="flex items-center justify-center m-5" to={'/cart'}>
-                  <button className="rounded-xl px-2 pb-1 text-sm">
-                      Adicionar ao carrinho
-                  </button>
-      </Link>
-      <div className="max-md:1300px overflow-auto">
-        <ul className="flex">
-          {data.data.map((item: any) => (
-            <li key={item.id} >
-              <div className={styles.menuItem}>
-                <img className={styles.menuItemImg} src={`src/shared/assets/images/${item.name}.png`}></img>
-                <p className={styles.menuItemName}>{item.name}</p>
-                <p className={styles.menuItemPrice}>{item.price}</p>
-                <Link className="bg-white" to={'/cart'}>
-                  <button className="bg-purus-button rounded-xl px-2 pb-1 text-sm">
-                      Adicionar ao carrinho
-                  </button>
-                </Link>
+    <div className={styles['menu-page']}>
+
+      {restaurantData.data.map((place: any) => ( 
+        <div>
+          <div className="flex items-center justify-center mt-10">
+            <img src="src\shared\assets\images\Subtract.png" alt="Imagem" className="mr-4" />
+            <h1 className="text-xl font-bold">{place.restaurantName}</h1>
+          </div>
+          <div className="mt-3">
+            <button className={styles['discreet-button']}>Adicionar novo item</button>
+          </div>
+
+          <div className={styles['menu-items']}>
+            {data.data.map((item: any) => (
+              <div> 
+                { item.restaurantId == place.id && 
+                  <div className={styles['menu-item']} key={item.id}>
+                    <img src={`src/shared/assets/images/${item.name}.png`} alt={item.name} />
+                    <p className={styles['menu-item-name']}>{item.name}</p>
+                    <p className={styles['menu-item-price']}>{item.price}</p>
+                    <Link to={`/cart/${item.id}`}>
+                      <button className={styles['add-to-cart-button']}>Adicionar ao carrinho</button>
+                    </Link>
+                  </div>
+                }
               </div>
-            </li>
-          ))}
-        </ul>
-      </div>
+            ))}
+          </div>
+        </div> ))}
+      
+      
     </div>
   );
 };
