d2e5a8316e3e7aacd2fd4d13c40be5fa6ba5e1d1
marcusv-fs <marcusv_fs@hotmail.com>
2023-09-12 11:49:42 -0300

    feat(PedidosContext): Adicionando Contexto na aplicação

--- frontend/src/app/home/pages/novoPedido/index.tsx
+++ frontend/src/app/home/pages/novoPedido/index.tsx
@@ -2,15 +2,18 @@ import styles from "./NovoPedido.module.css";
 import Header from "../../../../shared/components/Pedidos/Header";
 import Footer from "../../../../shared/components/Pedidos/Footer";
 import Produtos from "../../../../shared/components/Pedidos/Produtos";
+import Provider from "../../context/PedidosContext/Provider.jsx";
 
 
 
 const NovoPedido = () => {
     return (
         <div className={styles.body}>
-            <Header></Header>
-            <Produtos/>
-            <Footer></Footer>
+            <Provider>
+                <Header />
+                <Produtos />
+                <Footer />
+            </Provider>
         </div>
     );
 };
