e9a2d0c62f2d944f0000fc4f2872e6f74ef445db
marinavelosom <engmarinamelo@gmail.com>
2023-09-13 10:21:59 -0300

    feature/restaurant: integrate with back

--- frontend/src/app/home/components/Restaurants/Form/index.tsx
+++ frontend/src/app/home/components/Restaurants/Form/index.tsx
@@ -1,7 +1,50 @@
 import styles from "./index.module.css";
 import LogoTemporaria from "../../../../../shared/assets/logoTemp.svg";
+import { useState } from "react";
 
 const Form = () => {
+  const [formData, setFormData] = useState({
+    typeBusiness:'',
+    responsibleName: '',
+    responsibleCPF: '',
+    email: '',
+    phone: '',
+    password: '',
+    address: '',
+    phoneRestaurant: '',
+    corporateName: '',
+    restaurantName: '',
+    CNPJ: '',
+    speciality: ''
+  });
+
+  const handleInputChange = (event, name) => {
+    setFormData({ 
+      ...formData, 
+      [name]: event.target.value 
+    });
+  }
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleForm = async (event) => {
+    try {
+      event.preventDefault();
+      const response = await fetch('http://localhost:5001/api/restaurants', {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+      const data = await response.json();
+      window.open(`/login`, '_self');
+      alert("Cadastro realizado com sucesso!! Faça seu login");
+      // setShowPopup(true);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return(
     <section className={styles.container}>
       <div className={styles.header}>
@@ -10,13 +53,16 @@ const Form = () => {
       </div>
       <div className={styles.background}>
         <h2 className={styles.subtitle}>Dados da pessoa responsável</h2>
-        <div className={styles.formInputs}>
+        <form className={styles.formInputs} onSubmit={handleForm}>
           <div className={styles.inputContainer}>
             <label>Nome Completo</label>
             <input 
               name="name"
               placeholder="Digite seu nome completo"
               className={styles.input}
+              required
+              value = {formData.responsibleName}
+              onChange={(event) => handleInputChange(event, 'responsibleName')}
             />
           </div>
           <div className={styles.inputContainer}>
@@ -25,6 +71,9 @@ const Form = () => {
               name="email"
               placeholder="Digite seu melhor email"
               className={styles.input}
+              required
+              value = {formData.email}
+              onChange={(event) => handleInputChange(event, 'email')}
             />
           </div>
           <div className={styles.inputContainer}>
@@ -33,9 +82,47 @@ const Form = () => {
               name="phone"
               placeholder="Digite seu numero"
               className={styles.input}
+              required
+              value = {formData.phone}
+              onChange={(event) => handleInputChange(event, 'phone')}
             />
           </div>
-        </div>
+          <div className={styles.inputContainer}>
+            <label>CPF</label>
+            <input 
+              name="cpf"
+              placeholder="Digite seu CPF"
+              className={styles.input}
+              required
+              value = {formData.responsibleCPF}
+              onChange={(event) => handleInputChange(event, 'responsibleCPF')}
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <label>Senha</label>
+            <input 
+              type="password"
+              name="password"
+              placeholder="Digite sua senha"
+              className={styles.input}
+              required
+              value = {formData.password}
+              onChange={(event) => handleInputChange(event, 'password')}
+            />
+            {/* falta visualizar senha */}
+          </div>
+          {/* <div className={styles.inputContainer}>
+            <label>Confirme sua senha</label>
+            <input 
+              type="password"
+              name="password"
+              placeholder="Confirme sua senha"
+              className={styles.input}
+              required
+              value = {formData.password}
+              onChange={(event) => handleInputChange(event, 'password')}
+            />
+          </div> */}
         <h2 className={styles.subtitle}>Dados do restaurante</h2>
         <div className={styles.formInputs}>
           <div className={styles.inputContainer}>
@@ -44,6 +131,20 @@ const Form = () => {
               name="restaurantName"
               placeholder="Digite nome do restaurante"
               className={styles.input}
+              required
+              value = {formData.restaurantName}
+              onChange={(event) => handleInputChange(event, 'restaurantName')}
+            />
+          </div>
+          <div className={styles.inputContainer}>
+            <label>Tipo de negógico</label>
+            <input 
+              name="typeBusiness"
+              placeholder="Restaurante, farmácia, mercado..."
+              className={styles.input}
+              required
+              value = {formData.typeBusiness}
+              onChange={(event) => handleInputChange(event, 'typeBusiness')}
             />
           </div>
           <div className={styles.inputContainer}>
@@ -52,6 +153,9 @@ const Form = () => {
               name="phoneRestaunrant"
               placeholder="Telefone do restaurante"
               className={styles.input}
+              required
+              value = {formData.phoneRestaurant}
+              onChange={(event) => handleInputChange(event, 'phoneRestaurant')}
             />
           </div>
           <div className={styles.inputContainer}>
@@ -60,6 +164,9 @@ const Form = () => {
               name="cnpj"
               placeholder="CNPJ da empresa"
               className={styles.input}
+              required
+              value = {formData.CNPJ}
+              onChange={(event) => handleInputChange(event, 'CNPJ')}
             />
           </div>
           <div className={styles.inputContainer}>
@@ -68,10 +175,14 @@ const Form = () => {
               name="speciality"
               placeholder="Especialidade do restaurante"
               className={styles.input}
+              // required
+              value = {formData.speciality}
+              onChange={(event) => handleInputChange(event, 'speciality')}
             />
           </div>
         </div>
-        <button className={styles.button}>Cadastrar</button>
+        <button className={styles.button} type = "submit">Cadastrar</button>
+        </form>
       </div>
     </section>
   )
